504e437cf3ceca2d57ee2a6c2679701598043383
李迅 <lixun@mucang.cn>
2014-08-06 10:21:30 +0800

    Create project.

---
+++ jsCompressor/jsCompressor/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace jsCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFolderBtn = new System.Windows.Forms.Button();
+            this.FolderPathLab = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenFolderBtn
+            // 
+            this.OpenFolderBtn.Location = new System.Drawing.Point(12, 12);
+            this.OpenFolderBtn.Name = "OpenFolderBtn";
+            this.OpenFolderBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenFolderBtn.TabIndex = 0;
+            this.OpenFolderBtn.Text = "选择文件夹";
+            this.OpenFolderBtn.UseVisualStyleBackColor = true;
+            this.OpenFolderBtn.Click += new System.EventHandler(this.OpenFolderBtn_Click);
+            // 
+            // FolderPathLab
+            // 
+            this.FolderPathLab.AutoSize = true;
+            this.FolderPathLab.Location = new System.Drawing.Point(93, 17);
+            this.FolderPathLab.Name = "FolderPathLab";
+            this.FolderPathLab.Size = new System.Drawing.Size(0, 12);
+            this.FolderPathLab.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 326);
+            this.Controls.Add(this.FolderPathLab);
+            this.Controls.Add(this.OpenFolderBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFolderBtn;
+        private System.Windows.Forms.Label FolderPathLab;
+    }
+}
+
---
+++ jsCompressor/jsCompressor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jsCompressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFolderBtn_Click(object sender, EventArgs e)
+        {
+            // popup select folder dialog and remove create new folder btn.
+            var folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+
+            var result = folderDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                var folderPath = folderDialog.SelectedPath;
+                FolderPathLab.Text = folderPath;
+
+
+                // files in directory
+                int x = 10;
+                var files = Directory.EnumerateFiles(folderPath)
+                                 .OrderByDescending(filename => filename).ToList();
+
+                var i = 0;
+
+                var test = Directory.GetFileSystemEntries(folderPath, "*.png", SearchOption.AllDirectories);
+
+                var j = 0;
+
+                //string destination_dir = System.IO.Directory.GetCurrentDirectory() + @"./4x6";
+
+                //string[] extensions = { ".jpg", ".gif", ".png", ".tiff" };
+
+                //string[] dizin = Directory.GetFiles(fbd.SelectedPath, "*.*")
+                //    .Where(f => extensions.Contains(new FileInfo(f).Extension.ToLower())).ToArray();
+
+                //t4x6Folder.Text = dizin.Count().ToString();
+
+                //foreach (string file_name in dizin)
+                //{
+                //    File.Copy(file_name, destination_dir + file_name.Substring(fbd.SelectedPath.Length), true);
+                //}
+
+                //int fileCount = Directory.GetFiles(destination_dir, "*.*", SearchOption.TopDirectoryOnly).Length;
+
+                //t4x6Folder.Text = filepreCount.ToString();// fileCount.ToString();
+
+            }
+
+            folderDialog.Dispose();
+        }
+    }
+}
